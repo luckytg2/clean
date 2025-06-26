29b02140239fd78c6662ebaa09aba3f31ea907c7
luckytg2 <providersmmin@gmail.com>
2025-06-26 05:57:12 +0530

    Update cleaner_bot.py

--- cleaner_bot.py
+++ cleaner_bot.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 import asyncio
 import os
 
-API_ID = int(os.getenv("API_ID"))
+API_ID = int(os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
